a102f5d7f7693b2573eb7c8a6116b114312ab11c
Mateus Torres <mateus.ferreira@soulasalle.com.br>
2020-08-30 23:53:23 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,46 @@
+function AdicionarValorNaLista(colecao, valor)
+  table.insert(colecao, tonumber(valor));
+end
+
+function VerificarSeValorEstaNaLista(valor, colecao)
+
+  for chave, elemento in pairs(colecao) do
+    if elemento == valor then
+      return;
+    end
+  end
+
+  AdicionarValorNaLista(colecao, valor);
+
+end
+
+function ValidarValorInformado(valor, colecao)
+ 
+  if type(tonumber(valor)) == "number" then
+    VerificarSeValorEstaNaLista(valor, colecao)
+  end    
+end
+
+function OrdenarLista(colecao)
+ table.sort(colecao)
+end
+
+function ExibirSegundoMaiorElemento(colecao)
+  OrdenarLista(colecao);
+
+  local ultimoIndex = table.getn(colecao);
+
+  print(colecao[ultimoIndex - 1])
+end
+
+
+
+local lista = {};
+print(101 > 99)
+for i = 1, 10 do
+  io.write("Informe um valor\n");
+  local valor = io.read();
+  ValidarValorInformado(valor, lista);
+end
+
+ExibirSegundoMaiorElemento(lista);
